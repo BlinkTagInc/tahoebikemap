09f4c777dec98f88b9c1a34f41e3ce79fc1e71b4
Brendan Nee <brendan@blinktag.com>
2016-06-24 05:52:11 +0800

    sendgrid

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const nconf = require('nconf');
-const fetch = require('node-fetch');
+const mailHelper = require('sendgrid').mail;
+const _ = require('underscore');
 
 exports.index = (req, res, next) => {
   res.render('index');
@@ -8,5 +9,34 @@ exports.index = (req, res, next) => {
 exports.feedback = (req, res, next) => {
   console.log(req.body);
 
-  res.render('thankyou');
+  if (!req.body) {
+    next(new Error('No body sent'));
+  }
+
+  const text = _.reduce(req.body, (memo, value, key) => {
+    if (value) {
+      memo.push(`${key}: ${value}\n\n`);
+    }
+
+    return memo;
+  }, []).join('');
+
+  const from_email = new mailHelper.Email("noreply@tahoebike.org");
+  const to_email = new mailHelper.Email("brendan+tahoe@blinktag.com");
+  const subject = "Feedback from Lake Tahoe Bike Mapper";
+  const content = new mailHelper.Content("text/plain", text);
+  const mail = new mailHelper.Mail(from_email, subject, to_email, content);
+
+  const sg = require('sendgrid').SendGrid(nconf.get('SENDGRID_API_KEY'));
+  const requestBody = mail.toJSON();
+  const request = sg.emptyRequest();
+  request.method = 'POST';
+  request.path = '/v3/mail/send';
+  request.body = requestBody;
+  sg.API(request, (response) => {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+    res.render('thankyou');
+  });
 }
